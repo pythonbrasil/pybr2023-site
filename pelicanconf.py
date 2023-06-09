d00ba4e0a656b8d5b9848bc99c13ae031f46f9fd
Perceu Bertoletti <perceubertoletti@gmail.com>
2023-06-09 13:51:23 -0300

    ajust in templates and features from pelican

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Rede Neural'
+AUTHOR = 'Python Brasil'
 
-SITENAME = 'PyCaxias 2023'
+SITENAME = 'Python Brasil 2023'
 SITEYEAR = 2023
 
 SITEURL = '/'
@@ -26,40 +26,38 @@ FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 # OLD_EVENTS
-OLD_EVENTS = ()
+OLD_EVENTS = []
 # OLD_EVENTS
-MENU = (
-    ('#intro','Início', True),
-    ('#about','Sobre', False),
-    ('#schedule','Agenda', False),
-    ('#inscricao','Inscrição', False),
-    ('#supporters','Patrocinadores', False),
-    ('#contact','Contato', False),
-)
+MENU = [
+    ('#inicio', 'Início', True),
+    ('#evento', 'Sobre o evento', True),
+    ('#keynotes', 'Keynotes', True),
+    ('#programacao', 'Programação', True),
+    ('#trabalhos', 'Chamada de Trabalhos', True),
+    ('#patrocinadores', 'Patrocinadores', True),
+]
 
 # Social widget
 SOCIAL = {
-    'instagram':'https://www.instagram.com/pycaxias',
-    'facebook':'https://www.facebook.com/pycaxias',
+    'twitter':'https://twitter.com/pythonbrasil',
+    'instagram':'https://instagram.com/pythonbrasil',
+    'linkedin':'https://www.linkedin.com/company/apyb/',
+    'youtube':'https://www.youtube.com/c/pythonbrasiloficial',
+    'facebook':'https://www.facebook.com/pythonbrasil',
+    'flickr':'https://www.flickr.com/pythonbrasil',
+    'telegram':'https://t.me/pythonbrasil',
+    'email':'mailto:eventos@python.org.br',
 }
 
 DEFAULT_PAGINATION = False
-SITE_META_KEYWORDS = f"PyCaxias {SITEYEAR}, evento python, caxias do sul, evento caxias do sul, python, pycaxias, comunidade python caxias do sul, python rio grande do sul, comunidade"
-SITE_META_DESCRIPTION = "Evento da comunidade Python de Caxias do Sul, com intuito de popularizar e disseminar o conhecimento da linguagem python"
+SITE_META_KEYWORDS = f"Python Brasil, Python Brasil {SITEYEAR}, evento python, comunidade, evento, Programação, Python"
+SITE_META_DESCRIPTION = "Python Brasil é o maior evento sobre linguagem de programação Python do Brasil. Feito pela comunidade para a comunidade, tem o objetivo de difundir a linguagem, promover a troca de experiências e manter a comunidade crescendo igualmente em público e impacto social."
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
 EVENTO = {
-    "data": "11/03/2023",
-    "local": "11 de Março, Universidade de Caxias do Sul, Caxias do Sul",
-    "auditorio": "11 de Março, Universidade de Caxias do Sul, Caxias do Sul",
-    
-    "cadastramento_inicio": "11 de Março, Universidade de Caxias do Sul, Caxias do Sul",
-    "cadastramento_fim": "11 de Março, Universidade de Caxias do Sul, Caxias do Sul",
-    "Cadastramento_link": 'https://www.sympla.com.br/pycaxias-2023__1873136?share_id=5rmrc',
-
-    "c4f_inicio": "11 de Março, Universidade de Caxias do Sul, Caxias do Sul",
-    "c4f_fim": "11 de Março, Universidade de Caxias do Sul, Caxias do Sul",
-    "c4f_link": "11 de Março, Universidade de Caxias do Sul, Caxias do Sul",
+    'inscricao':'https://www.eventbrite.com.br/e/python-brasil-2023-registration-576537738897',
+    'data_evento': 'DE 30 OUTUBRO À 05 NOVEMBRO',
+    'cidade': 'Caxias do Sul, RS'
 }
--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://pycaxias.com.br'
+SITEURL = 'https://pythonbrasil.org.br'
 THEME = 'theme'
 RELATIVE_URLS = False
 
