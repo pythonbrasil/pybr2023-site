b414d3c3cc000c2611867c2075926366190307a0
Perceu Bertoletti <perceubertoletti@gmail.com>
2023-09-12 09:25:02 -0300

    ajuste na url das imagens

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://pythonbrasil.org.br'
+SITEURL = 'https://2023.pythonbrasil.org.br'
 THEME = 'theme'
 RELATIVE_URLS = False
 
